892d69b8c865b839e6e8de656326d6fa1b473ae3
Someone8448 <96267197+Someone8448@users.noreply.github.com>
2023-10-25 17:22:51 -0700

    Update audio.user.js

--- audio.user.js
+++ audio.user.js
@@ -4,10 +4,14 @@
 // @match       https://mppclone.com/*
 // @match       https://mpp.8448.space/*
 // @match       https://multiplayerpiano.com/*
+// @match       https://multiplayerpiano.net/*
+// @match       https://multiplayerpiano.org/*
+// @match       https://piano.mpp.community/*
 // @match       https://mpp.autoplayer.xyz/*
 // @match       https://mpp.hyye.tk/*
 // @match       https://mppclone.hri7566.info/*
 // @match       https://piano.ourworldofpixels.com/*
+// @match       https://mpp.hri7566.info/*
 // @grant       none
 // @version     1.0
 // @author      -
